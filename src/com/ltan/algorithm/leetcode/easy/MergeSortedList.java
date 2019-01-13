b19fe92bf136644ca6b02ffd7a7da9272da5089a
tanlin <tanlin@tik.com>
2019-01-13 17:28:19 +0800

    merge and no extra value

--- src/com/ltan/algorithm/leetcode/easy/MergeSortedList.java
+++ src/com/ltan/algorithm/leetcode/easy/MergeSortedList.java
@@ -29,6 +29,7 @@ public class MergeSortedList {
     static class Solution {
         /**
          * todo use a loop
+         *
          * @see #mergeTwoLists2(ListNode, ListNode)
          */
         public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
@@ -55,17 +56,48 @@ public class MergeSortedList {
             if (l2 == null) return l1;
             if (l1.val == l2.val) {
                 // clear the same value
-                l1.next = mergeTwoLists(l1.next, l2.next);
+                l1.next = mergeTwoLists2(l1.next, l2.next);
                 return l1;
                 /*l2.next = mergeTwoLists(l1.next, l2.next);
                 return l2;*/
             } else if (l1.val < l2.val) {
-                l1.next = mergeTwoLists(l1.next, l2);
+                l1.next = mergeTwoLists2(l1.next, l2);
                 return l1;
             } else {
-                l2.next = mergeTwoLists(l1, l2.next);
+                l2.next = mergeTwoLists2(l1, l2.next);
                 return l2;
             }
+        }
+
+        /**
+         * may the best algorithm
+         */
+        public ListNode mergeTwoLists3(ListNode l1, ListNode l2) {
+            if (l1 == null) return l2;
+            if (l2 == null) return l1;
+            ListNode head = new ListNode(0);
+            ListNode cur = head;
+            while (l1 != null && l2 != null) {
+                if(l1.val == l2.val) {
+                    cur.next = l1;
+                    l1 = l1.next;
+                    l2 = l2.next;
+                } else if (l1.val < l2.val) {
+                    cur.next = l1;
+                    l1 = l1.next;
+                } else {
+                    cur.next = l2;
+                    l2 = l2.next;
+                }
+                cur = cur.next;
+            }
+            if (l1 != null) {
+                cur.next = l1;
+            }
+            if (l2 != null) {
+                cur.next = l2;
+            }
+            return head.next;
         }
     }
 
@@ -73,8 +105,8 @@ public class MergeSortedList {
         Solution s = new Solution();
         ListNode l1 = new ListNode(0);
         ListNode l2 = new ListNode(0);
-        generateTwoList(l1, 1, 2, 5, 6, 8);
-        generateTwoList(l2, 1, 3, 5, 8, 9);
+        generateTwoList(l1, 1, 2, 5, 6, 7, 8, 8, 10);
+        generateTwoList(l2, 1, 3, 5, 8, 9, 10);
 
         ListNode l3 = s.mergeTwoLists(l1, l2);
         printNodeList(l3);
